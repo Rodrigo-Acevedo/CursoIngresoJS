87d6d70214f22dad970aa92807750c938719b9d4
Rodrigo Acevedo <acevedo.ezequiel@outlook.com>
2020-07-15 14:39:35 -0300

    primer prompt

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -2,7 +2,17 @@
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{	
+	let nombre;   //variable declarada
+	
+	/*prompt: funcion nativa de javascript que nos abre una ventana emergente con una mensaje y 
+	una caja de texto embebida donde el usuario puede ingresar algo*/ 
+	
+	nombre = prompt ("Ingrese su nombre");	    //lo que esta entre parentesis es el parametro
+	
+	/*alert: funcion que nos abre una ventana emergente, que tiene un mensaje. Es una ventana modal, 
+	porque cuando esta no se puede interactuar con lo que esta en la pantalla, hasta que lo cierre*/
+	
+	alert(nombre);
 }
 
