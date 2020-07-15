75d1723cc6c72f99378eadff9fabac258f0b3ecd
Rodrigo Acevedo <acevedo.ezequiel@outlook.com>
2020-07-15 12:05:32 -0300

    Cambios para que el sume, reste, multiplique y divida

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,60 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let num1;
+	let num2;
+	let suma;
+	
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	suma = num1 + num2;
+
+	alert ("La suma es " + suma);
+
 }
 
 function restar()
 {
+	let num1;
+	let num2;
+	let resta;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resta = num1 - num2;
+
+	alert("La resta es " + resta);
 	
 }
 
 function multiplicar()
 { 
+	let num1;
+	let num2;
+	let multiplicacion;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	multiplicacion = num1 * num2;
+
+	alert("El producto de " + num1 +  " * " + num2 + " es " + multiplicacion);
 	
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let division;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	division = num1 / num2;
+
+	alert("La division de " + num1 + " / " + num2 + " es " + division);
 }
 
