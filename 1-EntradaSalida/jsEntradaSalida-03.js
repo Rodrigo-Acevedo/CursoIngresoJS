9c160043680d785e16be8cfad10085ace035e28a
Rodrigo Acevedo <acevedo.ezequiel@outlook.com>
2020-07-15 17:44:30 -0300

    primer dato por ID

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -3,10 +3,18 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	//reservo lugar en memoria para guardar el nombre del usuario
+	let nombre; 
+	
+	//tomo el valor de la caja de texto (id: txtIdNombre) y se lo asigno a la variable nombre
+	nombre = document.getElementById("txtIdNombre").value   //txtIdNombre es el id (identificador)de la caja de texto
 
+	alert(nombre);
+
+
+	document.getElementById("txtIdNombre").value = "";  // ""  cadena vacia, para limpiar la caja de texto
 }
+
+
 
 
